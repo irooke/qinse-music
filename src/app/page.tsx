206856d445eb8314a724010f2bc0df86f2ea214a
irooke <ARookieA@outlook.com>
2024-05-22 01:38:37 +0800

    🔨 chore: 解决pr失败，删除全面检查中的报错代码

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { SongCover } from "@/components/song/cover";
+
 import PageContent from "@/components/homepage/pagecontent";
 
 export default function Home() {
   return (
-    <main className=" flex flex-col items-center px-40  pt-20">
+    <main className=" flex flex-col items-center px-40 pt-20">
       <div className="flex min-h-screen items-center justify-center bg-background p-5">
         <PageContent />
       </div>
